c05ac9d0a460cd5b1fc136e1ea9fed549d6b35e1
Dmytro Piskun <Dmytro.Piskun@caspio.com>
2018-12-13 17:17:36 +0200

    Fix gallery navigation

--- NeuralNetworkForms/Form5.Designer.cs
+++ NeuralNetworkForms/Form5.Designer.cs
@@ -28,102 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
-            this.p1 = new System.Windows.Forms.PictureBox();
-            this.p2 = new System.Windows.Forms.PictureBox();
-            this.a2 = new System.Windows.Forms.PictureBox();
-            this.a1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.a2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.a1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // p1
-            // 
-            this.p1.Location = new System.Drawing.Point(12, 73);
-            this.p1.Name = "p1";
-            this.p1.Size = new System.Drawing.Size(400, 350);
-            this.p1.TabIndex = 0;
-            this.p1.TabStop = false;
-            // 
-            // p2
-            // 
-            this.p2.Location = new System.Drawing.Point(418, 73);
-            this.p2.Name = "p2";
-            this.p2.Size = new System.Drawing.Size(400, 350);
-            this.p2.TabIndex = 1;
-            this.p2.TabStop = false;
-            // 
-            // a2
-            // 
-            this.a2.Image = ((System.Drawing.Image)(resources.GetObject("a2.Image")));
-            this.a2.Location = new System.Drawing.Point(344, 430);
-            this.a2.Name = "a2";
-            this.a2.Size = new System.Drawing.Size(68, 73);
-            this.a2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.a2.TabIndex = 2;
-            this.a2.TabStop = false;
-            this.a2.Click += new System.EventHandler(this.a2_Click_1);
-            // 
-            // a1
-            // 
-            this.a1.Image = ((System.Drawing.Image)(resources.GetObject("a1.Image")));
-            this.a1.Location = new System.Drawing.Point(418, 430);
-            this.a1.Name = "a1";
-            this.a1.Size = new System.Drawing.Size(68, 73);
-            this.a1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.a1.TabIndex = 3;
-            this.a1.TabStop = false;
-            this.a1.Click += new System.EventHandler(this.pictureBox4_Click);
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(12, 430);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(68, 73);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox5.TabIndex = 4;
-            this.pictureBox5.TabStop = false;
-            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label1.Location = new System.Drawing.Point(385, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 24);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // Form5
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 511);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.a1);
-            this.Controls.Add(this.a2);
-            this.Controls.Add(this.p2);
-            this.Controls.Add(this.p1);
-            this.Name = "Form5";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Галерея";
-            this.Load += new System.EventHandler(this.Form5_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.a2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.a1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+				System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+				this.p1 = new System.Windows.Forms.PictureBox();
+				this.p2 = new System.Windows.Forms.PictureBox();
+				this.arrowDown = new System.Windows.Forms.PictureBox();
+				this.arrowUp = new System.Windows.Forms.PictureBox();
+				this.pictureBox5 = new System.Windows.Forms.PictureBox();
+				this.label1 = new System.Windows.Forms.Label();
+				((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+				((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+				((System.ComponentModel.ISupportInitialize)(this.arrowDown)).BeginInit();
+				((System.ComponentModel.ISupportInitialize)(this.arrowUp)).BeginInit();
+				((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+				this.SuspendLayout();
+				// 
+				// p1
+				// 
+				this.p1.Location = new System.Drawing.Point(12, 73);
+				this.p1.Name = "p1";
+				this.p1.Size = new System.Drawing.Size(400, 350);
+				this.p1.TabIndex = 0;
+				this.p1.TabStop = false;
+				// 
+				// p2
+				// 
+				this.p2.Location = new System.Drawing.Point(418, 73);
+				this.p2.Name = "p2";
+				this.p2.Size = new System.Drawing.Size(400, 350);
+				this.p2.TabIndex = 1;
+				this.p2.TabStop = false;
+				// 
+				// arrowDown
+				// 
+				this.arrowDown.Image = ((System.Drawing.Image)(resources.GetObject("arrowDown.Image")));
+				this.arrowDown.Location = new System.Drawing.Point(344, 430);
+				this.arrowDown.Name = "arrowDown";
+				this.arrowDown.Size = new System.Drawing.Size(68, 73);
+				this.arrowDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+				this.arrowDown.TabIndex = 2;
+				this.arrowDown.TabStop = false;
+				this.arrowDown.Click += new System.EventHandler(this.arrowDown_Click);
+				// 
+				// arrowUp
+				// 
+				this.arrowUp.Image = ((System.Drawing.Image)(resources.GetObject("arrowUp.Image")));
+				this.arrowUp.Location = new System.Drawing.Point(418, 430);
+				this.arrowUp.Name = "arrowUp";
+				this.arrowUp.Size = new System.Drawing.Size(68, 73);
+				this.arrowUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+				this.arrowUp.TabIndex = 3;
+				this.arrowUp.TabStop = false;
+				this.arrowUp.Click += new System.EventHandler(this.arrowUp_Click);
+				// 
+				// pictureBox5
+				// 
+				this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+				this.pictureBox5.Location = new System.Drawing.Point(12, 430);
+				this.pictureBox5.Name = "pictureBox5";
+				this.pictureBox5.Size = new System.Drawing.Size(68, 73);
+				this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+				this.pictureBox5.TabIndex = 4;
+				this.pictureBox5.TabStop = false;
+				this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+				// 
+				// label1
+				// 
+				this.label1.AutoSize = true;
+				this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+				this.label1.Location = new System.Drawing.Point(385, 29);
+				this.label1.Name = "label1";
+				this.label1.Size = new System.Drawing.Size(60, 24);
+				this.label1.TabIndex = 5;
+				this.label1.Text = "label1";
+				this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+				// 
+				// Form5
+				// 
+				this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+				this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+				this.ClientSize = new System.Drawing.Size(836, 511);
+				this.Controls.Add(this.label1);
+				this.Controls.Add(this.pictureBox5);
+				this.Controls.Add(this.arrowUp);
+				this.Controls.Add(this.arrowDown);
+				this.Controls.Add(this.p2);
+				this.Controls.Add(this.p1);
+				this.Name = "Form5";
+				this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+				this.Text = "Галерея";
+				this.Load += new System.EventHandler(this.Form5_Load);
+				((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+				((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+				((System.ComponentModel.ISupportInitialize)(this.arrowDown)).EndInit();
+				((System.ComponentModel.ISupportInitialize)(this.arrowUp)).EndInit();
+				((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+				this.ResumeLayout(false);
+				this.PerformLayout();
 
         }
 
@@ -131,8 +131,8 @@
 
         private System.Windows.Forms.PictureBox p1;
         private System.Windows.Forms.PictureBox p2;
-        private System.Windows.Forms.PictureBox a2;
-        private System.Windows.Forms.PictureBox a1;
+        private System.Windows.Forms.PictureBox arrowDown;
+        private System.Windows.Forms.PictureBox arrowUp;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.Label label1;
     }
--- NeuralNetworkForms/Form5.cs
+++ NeuralNetworkForms/Form5.cs
@@ -6,69 +6,43 @@ namespace NeuralNetworkForms
 	public partial class Form5 : Form
 	{
 		private readonly Form3 _form3;
+		private readonly string _path = Application.StartupPath + "\\Photo\\";
 
-		private int n, nMain;
+		private int _pictureNumber;
 
 		public Form5(Form3 form3)
 		{
 			_form3 = form3;
-
 			InitializeComponent();
-
-			var path = Application.StartupPath + "\\Photo\\";
-			p1.Load(path + "//p1//1.jpg");
-			label1.Text = "Порівняння обробки фільтром та мережею";
-			p2.Load(path + "//p2//1.jpg");
-		}
-
-		private void LoadPic()
-		{
-			string st;
-			st = nMain + ".jpg";
-			var path = Application.StartupPath + "\\Photo\\";
-			p1.Load(path + "//p1//" + st);
-			p2.Load(path + "//p2//" + st);
 		}
 
 		private void Form5_Load(object sender, EventArgs e)
 		{
-			nMain = 3;
-			n = 1;
-			var path = Application.StartupPath;
-			LoadPic();
+			_pictureNumber = 0;
+			SetPictures(_pictureNumber);
 		}
 
-		private void a1_Click(object sender, EventArgs e)
+		private void arrowUp_Click(object sender, EventArgs e)
 		{
-			var path = Application.StartupPath;
-			nMain = n;
-			p1.Load(path + nMain + ".jpg");
-			p2.Load(path + nMain + ".jpg");
-			if (nMain < 4)
-				label1.Text = "Порівняння обробки фільтрами та мережами";
-			else
-				label1.Text = "Імітація авторського стилю";
+			if (_pictureNumber < 4)
+				_pictureNumber++;
+
+			SetPictures(_pictureNumber);
 		}
 
-		private void a2_Click(object sender, EventArgs e)
+		private void arrowDown_Click(object sender, EventArgs e)
 		{
-			var path = Application.StartupPath + "\\Photo\\";
-			nMain = n + 1;
-			p1.Load(path + nMain + ".jpg");
-			p2.Load(path + nMain + ".jpg");
-			if (nMain < 4)
-				label1.Text = "Порівняння обробки фільтрами та мережами";
-			else
-				label1.Text = "Імітація авторського стилю";
+			if (_pictureNumber > 0)
+				_pictureNumber--;
+
+			SetPictures(_pictureNumber);
 		}
 
-		private void a2_Click_1(object sender, EventArgs e)
+		private void SetPictures(int pictNumber)
 		{
-			var path = Application.StartupPath + "\\Photo\\";
-			nMain = n;
-			p1.Load(path + "//p1//" + nMain + ".jpg");
-			p2.Load(path + "//p2//" + nMain + ".jpg");
-			if (nMain < 4)
+			p1.Load(_path + "//p1//" + pictNumber + ".jpg");
+			p2.Load(_path + "//p2//" + pictNumber + ".jpg");
+			if (pictNumber < 4)
 				label1.Text = "Порівняння обробки фільтрами та мережами";
 			else
 				label1.Text = "Імітація авторського стилю";
@@ -78,18 +52,6 @@ namespace NeuralNetworkForms
 		{
 			_form3.Show();
 			Hide();
-		}
-
-		private void pictureBox4_Click(object sender, EventArgs e)
-		{
-			var path = Application.StartupPath + "\\Photo\\";
-			nMain = n + 1;
-			p1.Load(path + "//p1//" + nMain + ".jpg");
-			p2.Load(path + "//p2//" + nMain + ".jpg");
-			if (nMain < 4)
-				label1.Text = "Порівняння обробки фільтрами та мережами";
-			else
-				label1.Text = "Імітація авторського стилю";
 		}
 	}
 }
